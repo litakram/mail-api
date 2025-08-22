5eece19258b828cbad92609c20db9bded8106354
Akram <litnitiakram1@gmail.com>
2025-08-22 15:43:39 +0100

    Add files via upload

---
+++ api/send-email.js
@@ -0,0 +1,52 @@
+// api/send-email.js
+const nodemailer = require('nodemailer');
+
+export default async function handler(req, res) {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust '*' to specify allowed origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        // Preflight request
+        res.status(200).end();
+        return;
+    }
+
+    if (req.method === 'POST') {
+        const { to, cc, bcc, subject, message, isHtml } = req.body;
+
+        if (!to || !subject || !message) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+
+        // Configure Nodemailer
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'akramlitniti4@gmail.com',
+                pass: 'tadfzujsmyocnwpg'
+            }
+        });
+        
+        const mailOptions = {
+            from: 'akramlitniti4@gmail.com',
+            to: to,
+            cc: cc,
+            bcc: bcc,
+            subject: subject,
+            [isHtml ? 'html' : 'text']: message
+        };
+
+        
+        try {
+            await transporter.sendMail(mailOptions);
+            res.status(200).json({ success: 'Email sent successfully' });
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to send email' });
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
